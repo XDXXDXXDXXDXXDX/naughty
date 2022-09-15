c7aadcb9909c2838a807e53b887d6a6f6313a61d
谢栋祥 <dongxiang.xie@shopee.com>
2022-09-15 14:07:56 +0800

    init project
    Signed-off-by: 谢栋祥 <dongxiang.xie@shopee.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Title from './Title.vue';
+import {
+  Reading,
+  Delete,
+  FolderAdd,
+} from '@element-plus/icons-vue'
+</script>
+<template>
+  <el-row justify="space-between" align="middle" style="height: 44px" class="box">
+    <Title>NAUGHTY GOD</Title>
+    <p>Sign in</p>
+  </el-row>
+  <el-space direction="vertical" style="padding: 32px">
+    <el-button :icon="FolderAdd" circle color="var(--main-color)" plain />
+    <el-button :icon="Delete" circle />
+    <el-button :icon="Reading" circle />
+  </el-space>
+  <el-button class="create-btn" round color="var(--main-color)" plain size="large">CREATE</el-button>
+</template>
+<style scoped>
+.create-btn {
+  width: 210px;
+  position: fixed;
+  bottom: 96px;
+  left: 50%;
+  transform: translateX(-105px);
+}
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p class="title"><slot /></p>
+</template>
+
+<style scoped>
+  .title {
+    font-size: 24px;
+    font-weight: bold;
+  }
+</style>
